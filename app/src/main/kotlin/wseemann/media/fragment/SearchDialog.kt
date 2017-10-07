d142decf2da0d1deb4518afbf24c31626ab1ed4b
William Seemann <wseemann@pandora.com>
2017-10-06 17:19:10 -0700

    Added Kotlin support, initial code for search dialog

---
+++ app/src/main/kotlin/wseemann/media/fragment/SearchDialog.kt
@@ -0,0 +1,58 @@
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.SharedPreferences
+import android.preference.PreferenceManager
+import android.support.v4.app.DialogFragment
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
+
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+
+import wseemann.media.romote.R
+import wseemann.media.romote.model.Device
+import wseemann.media.romote.parser.DeviceInfoParser
+import wseemann.media.romote.utils.CommandHelper
+import wseemann.media.romote.utils.DBUtils
+import wseemann.media.romote.utils.PreferenceUtils
+
+class SearchDialog : DialogFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle): Dialog {
+        val inflater: LayoutInflater = activity.getLayoutInflater()
+
+        var view: View = inflater.inflate(R.layout.dialog_fragment_manual_connection, null)
+        var searchText: EditText = view.findViewById(R.id.ip_address_text) as EditText
+        var searchButton: Button = view.findViewById(R.id.connect_button) as Button
+
+        searchButton.setOnClickListener {
+                var ipAddressText: EditText = searchText
+
+                //sendCommand(CommandHelper.getDeviceInfoURL(ManualConnectionDialog.this.getContext(), mHost));
+        }
+
+        var builder: AlertDialog.Builder = AlertDialog.Builder(activity);
+        builder.setView(view);
+        builder.setTitle(getString(R.string.connect_manually));
+        builder.setMessage(getString(R.string.connect_help));
+        builder.setPositiveButton(R.string.install_channel_dialog_button, { dialog, id ->
+
+        })
+
+        return builder.create();
+    }
+}
