f231b09b5fb178ad226a14a46865f869969ba76e
anasofiagribeiro <asofiagribeiro@gmail.com>
2020-08-05 22:02:21 +0100

    Refactor4Green - Dynamic Retry Delay EP

--- app/src/main/java/wseemann/media/romote/service/CommandService.java
+++ app/src/main/java/wseemann/media/romote/service/CommandService.java
@@ -1,7 +1,13 @@
 package wseemann.media.romote.service;
 
+import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.Network;
 import android.util.Log;
 
 import com.jaku.core.JakuRequest;
@@ -31,13 +37,22 @@ public class CommandService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        Log.d(TAG, "onHandleIntent called");
+        if (hasActiveNetwork()) {
+            Log.d(TAG, "onHandleIntent called");
 
-        if (intent != null) {
-            //if (intent.getAction() != null) {
-                //Log.d(TAG, "onHandleIntent: " + intent.getAction());
-                performKeypress((KeypressKeyValues) intent.getSerializableExtra("keypress"));
-            //}
+            if (intent != null) {
+                //if (intent.getAction() != null) {
+                    //Log.d(TAG, "onHandleIntent: " + intent.getAction());
+                    performKeypress((KeypressKeyValues) intent.getSerializableExtra("keypress"));
+                //}
+            }
+        }
+        else {
+            NetworkStateReceiver networkStateReceiver = new NetworkStateReceiver();
+            ConnectivityManager connectivityManager = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+            networkStateReceiver.enable(getApplicationContext());
+            networkStateReceiver.setService(this);
+            connectivityManager.registerDefaultNetworkCallback(networkStateReceiver);
         }
     }
 
@@ -58,5 +73,60 @@ public class CommandService extends IntentService {
 
             }
         }).execute(RokuRequestTypes.keypress);
+    }
+
+    //The method hasActiveNetwork() checks whether the network connection is active
+    protected boolean hasActiveNetwork() {
+        final ConnectivityManager connManager = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        Network activeNetwork = connManager.getActiveNetwork();
+        return (activeNetwork != null);
+    }
+
+    public class NetworkStateReceiver extends ConnectivityManager.NetworkCallback {
+        private CommandService service;
+
+        public void setService(CommandService newService) {
+            service = newService;
+        }
+
+        @Override
+        public void onAvailable(Network network) {
+
+            // If there is an active network connection, this method will "turn off" this class and arrange to process the request
+            if (service.hasActiveNetwork()) {
+                Context context = getApplicationContext();
+                disable(context);
+                final AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+                final Intent innerIntent = new Intent(context, CommandService.class);
+                final PendingIntent pendingIntent = PendingIntent.getService(context, 0, innerIntent, 0);
+
+                SharedPreferences preferences = context.getSharedPreferences(context.getPackageName(), Context.MODE_PRIVATE);
+                preferences.edit();
+                boolean autoRefreshEnabled = preferences.getBoolean("pref_auto_refresh_enabled", false);
+
+                final String hours = preferences.getString("pref_auto_refresh_enabled", "0");
+                long hoursLong = Long.parseLong(hours) * 60 * 60 * 1000;
+
+                if (autoRefreshEnabled && hoursLong != 0) {
+                    final long alarmTime = preferences.getLong("last_auto_refresh_time", 0) + hoursLong;
+                    alarmManager.set(AlarmManager.RTC, alarmTime, pendingIntent);
+                } else {
+                    alarmManager.cancel(pendingIntent);
+                }
+            }
+        }
+
+        // Method to  "turn on" this class
+        public void enable(Context context) {
+            ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            connectivityManager.registerDefaultNetworkCallback(this);
+        }
+
+        // Method to  "turn off" this class
+        public void disable(Context context) {
+            ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            connectivityManager.unregisterNetworkCallback(this);
+        }
+
     }
 }
