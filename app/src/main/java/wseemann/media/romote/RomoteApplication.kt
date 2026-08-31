ffbaab906d4884ecceb3af8f3a34b2acb054ee3f
William Seemann <wseemann@gmail.com>
2026-08-31 02:17:52 -0500

    Inject AppPreferences early

--- app/src/main/java/wseemann/media/romote/RomoteApplication.kt
+++ app/src/main/java/wseemann/media/romote/RomoteApplication.kt
@@ -1,11 +1,17 @@
 package wseemann.media.romote
 
 import android.app.Application
+import android.content.SharedPreferences
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
+import javax.inject.Inject
 
 @HiltAndroidApp
 class RomoteApplication : Application() {
+
+    @Inject
+    lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate() {
         super.onCreate()
 
